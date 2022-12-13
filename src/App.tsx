e95948e75f27c502d84ce4f9edd3f7b0875fc681
Daniel Michalski <danielp.michalski@gmail.com>
2022-12-13 14:56:22 +0100

    added material ui package and implemented sliders backed by proportional-allocator

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DeleteOutlined from "@mui/icons-material/DeleteOutlined";
+import { Button, Grid } from "@mui/material";
+import Slider from "@mui/material/Slider";
+import { ProportionalAllocator } from "proportional-allocator";
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [sliders, setSliders] = React.useState<number[]>([]);
+  const allocator = new ProportionalAllocator();
+
+  const updateRawSliders = React.useCallback(() => {
+    setSliders(
+      allocator
+        .getAllocations()
+        .map((v) => parseFloat((v * 100).toPrecision(4)))
+    );
+  }, [setSliders]);
+
+  const addSlider = React.useCallback(() => {
+    allocator.push();
+    updateRawSliders();
+  }, [updateRawSliders]);
+
+  const updateSliders = React.useCallback(
+    (index: number, value: number) => {
+      allocator.update(index, value / 100);
+      updateRawSliders();
+    },
+    [updateRawSliders]
+  );
+
+  const onSliderChange = React.useCallback(
+    (index: number) => {
+      return (_: Event, value: number | number[]) => {
+        updateSliders(index, value as number);
+      };
+    },
+    [updateSliders]
+  );
+
+  const deleteSlider = React.useCallback(
+    (index: number) => {
+      allocator.remove(index);
+      updateRawSliders();
+    },
+    [updateRawSliders]
+  );
+
+  const toSlider = React.useCallback(
+    (value: number, index: number) => (
+      <>
+        <Grid item xs={10}>
+          <Slider
+            className="slider"
+            onChange={onSliderChange(index)}
+            key={index}
+            value={value}
+            aria-label="Default"
+            valueLabelDisplay="auto"
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <DeleteOutlined
+            className="delete-button"
+            onClick={() => deleteSlider(index)}
+          />
+        </Grid>
+      </>
+    ),
+    [deleteSlider, onSliderChange]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button variant="contained" onClick={addSlider}>
+        Add slider
+      </Button>
+      <Grid container spacing={2}>
+        {sliders.map(toSlider)}
+      </Grid>
+    </>
   );
 }
 
